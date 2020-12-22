7ff5552bd7bcaf01b3115c8d51a0dbba729f4dd2
Besim Gjidija <besimgjidija@gmail.com>
2020-12-22 13:11:02 -0500

    Uploading to github for help

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
-const btn_reset = document.querySelectorAll('.btn_reset');
+const btn_reset = document.querySelector('.btn__reset');
 
 const phrases = [
     "Mercury",
@@ -15,3 +15,24 @@ btn_reset.addEventListener("click", () => {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
+
+function getRandomPhraseAsArray(arr){
+    // generates a random number
+    const RandomArr = Math.floor( Math.random() * phrases.length);
+    return arr[RandomArr];
+};
+
+// console.log(phrases[getRandomPhraseAsArray]);
+console.log(getRandomPhraseAsArray(phrases));
+// getRandomPhraseAsArray(phrases[RandomArrlength]);
+
+function checkLetter(clicked) {
+const lis = document.getElementsByTagName('li');
+
+if (clicked == li) {
+   for(let i = 0; i < lis.length; i ++)
+   let lis[i] = li;
+   (null);
+};
+
+};
